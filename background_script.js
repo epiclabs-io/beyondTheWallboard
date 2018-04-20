55686824d24fe7432292f1c586cae6d6fe2d86a0
Lucas Bernalte <lucasb07@hotmail.com>
2018-04-20 17:55:53 +0200

    Add fullscreen config parameter.

--- background_script.js
+++ background_script.js
@@ -69,7 +69,7 @@ function closeAllTabs(config) {
 
 function createWindow(config) {
   var urls = config.tabs.map(tab => tab.url);
-  chrome.windows.create({url: urls}, function (win) {
+  chrome.windows.create({url: urls, state: config.general.fullScreen ? 'fullscreen' : 'normal'}, function (win) {
     for (var i=0; i< win.tabs.length; i++) {
       config.tabs[i].id = win.tabs[i].id;
     }
